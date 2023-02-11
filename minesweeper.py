bd84bee8ac9b5b95dd8d47521688db37ea828d80
Luca Cirillo <luca.cirillo5@gmail.com>
2023-02-11 20:59:38 +0100

    feat: create a Minesweeper window from Tk window

--- minesweeper.py
+++ minesweeper.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import os
+from tkinter import *
+
+
+class MinesweeperTk(Tk):
+    DEFAULT_ROWS: int = 8
+    DEFAULT_COLUMNS: int = 8
+    DEFAULT_MINES: int = 10
+
+    SPRITES_PATH: str = os.path.join("res", "emoji")
+
+    def __init__(self):
+        super().__init__()
+        # Set window title
+        self.title("Minesweeper")
+        # Center window
+        self.eval("tk::PlaceWindow . center")
+
+
+if __name__ == "__main__":
+    # Create a window with Tkinter
+    window = MinesweeperTk()
+    # Show window
+    window.mainloop()
