fb203acb80663b6bcc9074e29a687eb8ea4e5882
Luca Cirillo <luca.cirillo5@gmail.com>
2023-02-11 21:27:19 +0100

    add: mouse click bind to sample functions

--- minesweeper.py
+++ minesweeper.py
@@ -32,8 +32,20 @@ class MinesweeperTk(Tk):
             for j in range(self.DEFAULT_COLUMNS):
                 # Create a button
                 button = Button(self, image=self.sprite_blank, width=30, height=30)
+                # Bind an handler for mouse left-click on a cell, that 'open' the cell
+                button.bind(sequence="<Button-1>", func=self.open_cell)
+                # Bind an handler for mouse right-click on a cell that put a flag on the cell
+                button.bind(sequence="<Button-2>", func=self.put_flag)
                 # Place button in grid
                 button.grid(row=i, column=j)
+
+    def open_cell(self, event):
+        print(event.widget.tag)
+        event.widget.configure(image=self.sprite_numbers[0])
+        event.widget["state"] = "disabled"
+
+    def put_flag(self, event):
+        event.widget.configure(image=self.sprite_flag)
 
 
 if __name__ == "__main__":
